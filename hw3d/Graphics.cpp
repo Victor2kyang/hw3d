f8175c21d881e0c9d06618cee36b602b07e84912
TimeMosaic <timemosaic@foxmail.com>
2020-08-14 09:40:08 +0800

    [ywq0814] play graphics

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -107,16 +107,29 @@ void Graphics::DrawTestTriangle()
 	//建立cpu结构，准备传给gpu
 	struct  Vertex
 	{
-		float x;
-		float y;
+		struct{
+			float x;
+			float y;
+		}pos;
+		struct {
+			unsigned char r;
+			unsigned char g;
+			unsigned char b;
+			unsigned char a;
+		}color;
 	};
 	
 	//create vertex buffer(1 2d triangle at center of screen
-	const Vertex vertices[] = {
-		{ 0.0f,0.5f },
-		{ 0.5f,-0.5f },
-		{ -0.5f,-0.5f },
+	Vertex vertices[] = {
+		{ 0.0f,0.5f,255,0,0,0 },
+		{ 0.5f,-0.5f,0,255,0,0 },
+		{ -0.5f,-0.5f,0,0,255,0 },
+		{ -0.3f,0.3f,0,255,0,0 },
+		{ 0.3f,0.3f,0,0,255,0 },
+		{ 0.0f,-0.8f,255,0,0,0 },
 	};
+	//vertices[0].color.r = 155;
+	//vertices[0].color.g = 155;
 
 	//const Vertex vertices[] = {
 	//	{ 0.0f,0.5f },
@@ -141,7 +154,30 @@ void Graphics::DrawTestTriangle()
 	sd.pSysMem = vertices;
 	//创造一块buffer
 	GFX_THROW_INFO(pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer));
-	//pDevice->CreateBuffer();
+	
+	// create index buffer
+	const unsigned short indices[] =
+	{
+		0,1,2,
+		0,2,3,
+		0,4,1,
+		2,1,5,
+	};
+	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+	D3D11_BUFFER_DESC ibd = {};
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.CPUAccessFlags = 0u;
+	ibd.MiscFlags = 0u;
+	ibd.ByteWidth = sizeof(vertices);
+	ibd.StructureByteStride = sizeof(Vertex);
+	D3D11_SUBRESOURCE_DATA isd = {};
+	isd.pSysMem = indices;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
+
+	//bind index buffer
+	pContext->IASetIndexBuffer(pIndexBuffer.Get(),DXGI_FORMAT_R16_UINT,0u);
+
 	//IA打头的装配器相关 绑定顶点数据到pipeline
 	//Bind vertex buffer to pipeline
 	const UINT stride = sizeof(Vertex);
@@ -176,6 +212,7 @@ void Graphics::DrawTestTriangle()
 	const D3D11_INPUT_ELEMENT_DESC ied[] =
 	{
 		{"Position",0,DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
+		{"Color",0,DXGI_FORMAT_R8G8B8A8_UNORM,0,8u,D3D11_INPUT_PER_VERTEX_DATA,0},
 	};
 	GFX_THROW_INFO(pDevice->CreateInputLayout(
 		ied, (UINT)std::size(ied),
@@ -194,21 +231,25 @@ void Graphics::DrawTestTriangle()
 	//设置拓扑类型 Primitive Topologies 常见有点，线，三角形，
 	//Set primitive topology to triangle list(groups of 3 vertices
 	pContext->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	//pContext->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST);
+	//pContext->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINESTRIP);
 
 	//configure viewport
 	D3D11_VIEWPORT vp;
-	vp.Width = 800;
-	vp.Height = 600;
+	vp.Width = 400;
+	vp.Height = 300;
 	vp.MinDepth = 0;
 	vp.MaxDepth = 1;
-	vp.TopLeftX = 0;
-	vp.TopLeftY = 0;
+	vp.TopLeftX = 100;
+	vp.TopLeftY = 100;
 	pContext->RSSetViewports(1u, &vp);
 
 	//Draw参数是（顶点数量，顶点起始位置）
 	//pContext->Draw(3u, 0u);
 	//GFX_THROW_INFO_ONLY(pContext->Draw(3u, 0u));
-	GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
+	//GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
+	//索引绘制 indexed draw
+	GFX_THROW_INFO_ONLY(pContext->DrawIndexed((UINT)std::size(indices),0u, 0u));
 }
 
 
--- hw3d/VertexShader.hlsl
+++ hw3d/VertexShader.hlsl
@@ -1,4 +1,13 @@
-float4 main(float2 pos : Position) : SV_Position
+struct v2f
 {
-	return float4(pos.x,pos.y,0.0f,1.0f);
+	float3 color : Color;
+	float4 pos : SV_Position;
+};
+
+v2f main(float2 pos : Position,float3 color:Color)
+{
+	v2f v;
+	v.pos = float4(pos.x, pos.y, 0.0f, 1.0f);
+	v.color = color;
+	return v;
 }
