d9550f68b7a7c9ab6c9b4e3fcabb66ba53eb2e94
TimeMosaic <timemosaic@foxmail.com>
2020-08-18 09:31:08 +0800

    [ywq0818] set vertex shader
    &spin about time

--- hw3d/VertexShader.hlsl
+++ hw3d/VertexShader.hlsl
@@ -1,13 +1,18 @@
-struct v2f
+struct VSOut
 {
 	float3 color : Color;
 	float4 pos : SV_Position;
 };
 
-v2f main(float2 pos : Position,float3 color:Color)
+cbuffer CBuf
 {
-	v2f v;
-	v.pos = float4(pos.x, pos.y, 0.0f, 1.0f);
+    matrix transform;
+};
+
+VSOut main(float2 pos : Position, float3 color : Color)
+{
+    VSOut v;
+    v.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f),transform);
 	v.color = color;
 	return v;
 }
